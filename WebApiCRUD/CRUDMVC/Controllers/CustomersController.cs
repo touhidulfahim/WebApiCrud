8fc674d00b156763337b0a36692e247f8c19d8ef
Touhidul Fahim <muhdtohidul@gmail.com>
2018-08-13 16:25:30 +0600

    Add Ver 2.1

---
+++ WebApiCRUD/CRUDMVC/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using CRUDMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CRUDMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            IEnumerable<CustomerViewModel> empList;
+            HttpResponseMessage response = GlobalVariable.WebApiClient.GetAsync("Customer").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<CustomerViewModel>>().Result;
+            return View(empList);
+        }
+    }
+}
--- WebApiCRUD/WebApiCRUD/App_Start/WebApiConfig.cs
+++ WebApiCRUD/WebApiCRUD/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace WebApiCRUD
 {
@@ -24,7 +25,7 @@ namespace WebApiCRUD
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            );            
         }
     }
 }
